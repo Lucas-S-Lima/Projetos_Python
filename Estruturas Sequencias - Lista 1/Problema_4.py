d57a27a0f6e3d99b958ea357dcee16c8535ba540
Lucas da Silva Lima <2ucas2ima@gmail.com>
2023-05-08 13:20:23 -0300

    Incremento ao repositório

---
+++ Estruturas Sequencias - Lista 1/Problema_4.py
@@ -0,0 +1,8 @@
+n1 = float(input("Insira a primeira nota: "))
+n2 = float(input("Insira a segunda nota: "))
+n3 = float(input("Insira a terceira nota: "))
+n4 = float(input("Insira a quarta nota: "))
+
+media = (float(n1 + n2 + n3 + n4) / 2 )
+
+print(f"Sua nota média é {media:.2f}")
