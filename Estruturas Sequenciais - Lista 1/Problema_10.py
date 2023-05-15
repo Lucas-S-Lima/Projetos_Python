0c339c264b5585fb485dbd3ac708d7babbbf01fd
Lucas da Silva Lima <2ucas2ima@gmail.com>
2023-05-15 20:25:53 -0300

    Incremento ao repositório

---
+++ Estruturas Sequenciais - Lista 1/Problema_10.py
@@ -0,0 +1,4 @@
+c = float(input("Insira a temperatura em Celsius: "))
+fa =  ((9 * c / 5) + 32 )
+
+print("A temperatura em Farenheit é de %.2f" % fa, "°F")
---
+++ Estruturas Sequenciais - Lista 1/Problema_9.py
@@ -0,0 +1,4 @@
+fa = float(input("Insira a temperatura em Farenheit: "))
+celsius = 5 * ((fa - 32) / 9)
+
+print("A temperatura em Celsius é de %.2f" % celsius, "°C")
